300836b04f05a4c7cea05978899e10ed1e5c1540
amir.hussain <amir.hussain@mobigesture.com>
2020-07-29 14:12:52 +0530

    implemented routing and added snapshot testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 import './App.css';
+import Header from './components/Header/header';
+import Employees from './components/Employees/employees';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Redirect to='/employees' />
+          </Route>
+          <Route exact path='/employees' component={Employees} />
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
